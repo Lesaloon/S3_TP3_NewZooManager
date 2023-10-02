1c6bcddb464bc9fcb13c30060c79daf8bb02aa64
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2023-10-02 15:47:03 +0200

    Implementing the Dog class

--- TP3NewZooManager/Dog.cs
+++ TP3NewZooManager/Dog.cs
@@ -1,5 +1,11 @@
 namespace TP3NewZooManager; 
 
-public class Dog {
+public class Dog : Animal {
+	public Dog(string name, int age, string race) : base(name, age, race) {
+		
+	}
 	
+	public override void Manger(string food) {
+		Console.WriteLine("Wouf, miam");
+	}
 }
