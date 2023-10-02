57e205642c2b62cc1de2eefc4a08a8f118b64e18
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2023-10-02 15:46:30 +0200

    adding a Zoo var with mochup data

--- TP3NewZooManager/Program.cs
+++ TP3NewZooManager/Program.cs
@@ -1,8 +1,15 @@
 ﻿using TP3NewZooManager;
+using TP3NewZooManager.Interface;
 
 namespace TP3NewZooManager {
 	internal class Program {
+
 		static void Main(string[] args) {
+			List<IAnimal> Zoo = new List<IAnimal>();
+			
+			Zoo.Add(new Cat("jaque", 1, "mencoune"));
+			Zoo.Add(new MadCat("michel", 2, "mencoune", 10));
+			Zoo.Add(new Dog("Artyom", 3, "Husky"));
 			
 		}
 	}
